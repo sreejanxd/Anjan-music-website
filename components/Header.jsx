4eacdc4fab24798ba050693bbb2cac341a572f7e
Anjan-Dhar <anjandharsiliguri1@gmail.com>
2024-07-23 00:50:22 +0530

    ✨️🧑‍💻Anjan Dhar✨️🧑‍💻

---
+++ components/Header.jsx
@@ -0,0 +1,21 @@
+import { Button } from "./ui/button";
+import { Star } from "lucide-react";
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+import Logo from "./Logo";
+import Search from "./Search";
+
+export default function Header() {
+    return (
+        <header className="flex justify-between items-center py-5 md:px-20 lg:px-28 px-6">
+            <Logo />
+            <div className="flex items-center justify-center gap-2">
+                <div className="hidden md:block">
+                    <Search />
+                </div>
+                <ModeToggle />
+                <Button size="icon" asChild><Link href="https://github.com/r2hu1/musichub" target="_blank"><Star className="h-4 w-4"/></Link></Button>
+            </div>
+        </header>
+    )
+}
