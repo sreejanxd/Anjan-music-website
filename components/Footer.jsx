b34ce7a0163a2704c36cf3950d7e69382e0945b7
Anjan-Dhar <anjandharsiliguri1@gmail.com>
2024-07-23 17:34:00 +0530

    ✨️🧑‍💻Aɳʝαɳ ԃԋαɾ✨️🧑‍💻

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
             <p className="text-sm">Made ❤️ by <a className="hover:underline hover:text-primary" href="https://github.com/Siliguri-design">Anjan Dhar</a><span className="text-primary">.</span></p>
             <ul className="flex gap-1 items-center justify-center mt-1">
                 <li><a className="text-xs bg-secondary hover:bg-primary hover:text-white rounded-full p-1 px-2" href="https://instagram.com/badboyanj" target="_blank">Instagram</a></li>
-                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-white rounded-full p-1 px-2" href="https://github.com/Siliguri-design" target="_blank">Github</a></li>
+                <li><a className="text-xs bg-secondary hover:bg-primary hover:text-white rounded-full p-1 px-2" href="https://wa.me/917029666180?text= *✨️🧑‍💻𝘏𝘭𝘸 𝘈𝘯𝘫𝘢𝘯 𝘺𝘰𝘶𝘳 𝘴𝘰𝘯𝘨𝘩𝘶𝘣 𝘪𝘴 𝘢𝘸𝘦𝘴𝘰𝘮𝘦 𝘉𝘳𝘰𝘰𝘰✨️🧑‍💻*" target="_blank">Whatsapp</a></li>
             </ul>
         </footer>
     )
