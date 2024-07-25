598cde17ff856c924b8e9b67214f4c5f5844d493
Anjan-Dhar <anjandharsiliguri1@gmail.com>
2024-07-25 08:38:18 +0530

    ✨️🧑‍💻Aɳʝαɳ ԃԋαɾ✨️🧑‍💻

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-3xl font-bold">Music<span className="text-primary">hub</span></h1>
+                <h1 className="text-3xl font-bold">Anjan<span className="text-primary">Music</span></h1>
             </div>
         </Link>
     )
