95041c39a865af0108e0b58e73efee03f1c6e8a0
Sreejanxd🥵 <oyysreejan@gmail.com>
2025-01-06 20:54:14 +0530

    Update Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-3xl font-bold">Anjan<span className="text-primary">Music</span></h1>
+                <h1 className="text-3xl font-bold">Sreejan<span className="text-primary">Music</span></h1>
             </div>
         </Link>
     )
